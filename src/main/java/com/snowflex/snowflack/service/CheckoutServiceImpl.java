522cc12305a9a8abea11639809c3a48b8af1fe23
Hirusha-Kavinda <138157078+Hirusha-Kavinda@users.noreply.github.com>
2023-09-26 21:42:57 +0530

    implement orders post api

--- src/main/java/com/snowflex/snowflack/service/CheckoutServiceImpl.java
+++ src/main/java/com/snowflex/snowflack/service/CheckoutServiceImpl.java
@@ -3,9 +3,14 @@ package com.snowflex.snowflack.service;
 import com.snowflex.snowflack.dao.CustomerRepository;
 import com.snowflex.snowflack.dto.Purchase;
 import com.snowflex.snowflack.dto.PurchaseResponse;
+import com.snowflex.snowflack.entity.Customer;
 import com.snowflex.snowflack.entity.Order;
+import com.snowflex.snowflack.entity.OrderItem;
 import jakarta.transaction.Transactional;
 import org.springframework.stereotype.Service;
+
+import java.util.Set;
+import java.util.UUID;
 
 @Service
 public class CheckoutServiceImpl implements CheckoutService{
@@ -28,21 +33,30 @@ public class CheckoutServiceImpl implements CheckoutService{
         order.setOrderTrackingNumber(orderTrackingNumber);
 
         // populate order with orderItems
+        Set<OrderItem> orderItems = purchase.getOrderItems();
+        orderItems.forEach(item -> order.add(item));
 
         //populate order with billingAddress and shippingAddress
+        order.setBillingAddress(purchase.getBillingAddress());
+        order.setShippingAddress(purchase.getShippingAddress());
 
         // populate customer with order
+        Customer customer = purchase.getCustomer();
+        customer.add(order);
 
-        // save to the databse
+        // save to the database
+        customerRepository.save(customer);
 
         // return a response
+        return  new PurchaseResponse(orderTrackingNumber);
 
-        return null;
+
     }
 
     private String generateOrderTrackingNumber() {
 
         // generate a random UUID number (UUID Version-4)
         // For details see : https://en.
+        return UUID.randomUUID().toString();
     }
 }
